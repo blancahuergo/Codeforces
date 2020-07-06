1e5861a6d92114133fc164c6a49a41804dda83ae
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-07-06 17:17:24 +0200

    Add files via upload

---
+++ 1339A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int T, N;
+	cin >> T;
+	while(T--) {
+		cin >> N;
+		cout << N << "\n";
+	}
+	return 0;
+}
